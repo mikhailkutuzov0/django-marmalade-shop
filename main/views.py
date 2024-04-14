eb7378db8cc217f698b79a7af587b27d46d31914
kutuzwr <mikhailkutuzov0@gmail.com>
2024-04-14 15:25:31 +0300

    urlpatterns changed

--- main/views.py
+++ main/views.py
@@ -1,14 +1,18 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина',
+        'title': 'Home - Главная',
+        'content': 'marmelade-shop',
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('Home')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том что это за магазин '
+    }
+    return render(request, 'main/about.html', context)
