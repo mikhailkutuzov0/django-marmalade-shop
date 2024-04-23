46ab82465eb11fb69710d6d2c6ef97e9d5fc2ad4
kutuzwr <mikhailkutuzov0@gmail.com>
2024-04-24 02:52:11 +0300

    the ability to change the number of products in cart has been made

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse
-from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
 
 from carts.models import Cart
@@ -36,7 +35,26 @@ def cart_add(request):
 
 
 def cart_change(request):
-    ...
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quaantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_remove(request):
@@ -46,7 +64,9 @@ def cart_remove(request):
     cart.delete()
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+        "carts/includes/included_cart.html",
+        {"carts": user_cart}, request=request
+    )
 
     response_data = {
         "message": "Товар удален",
